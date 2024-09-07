d15a68563b8acf74e3094b791ac583df21518302
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-09-07 05:42:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,7 +36,7 @@ async def pm_search(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "sᴜᴘᴘᴏʀᴛ 😊", url=link
+                        "sᴜᴘᴘᴏʀᴛ 😊", url='https://t.me/+swpuQspBoVxiNjdl'
                     )
                 ]
             ]
