4c092fba04924d9f8a65dda98d6f341fb6a9ccb9
Safari <142569922+Safaridevv@users.noreply.github.com>
2024-11-02 10:46:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -315,8 +315,8 @@ async def start(client:Client, message):
             ]]
             toDel = await client.send_cached_media(
                 chat_id=message.from_user.id,
-                file_id=file.file_id
-                caption=f_caption,
+                file_id=file.file_id,
+                caption=f_caption
                 #reply_markup=InlineKeyboardMarkup(btn)
             )
             files_to_delete.append(toDel)
