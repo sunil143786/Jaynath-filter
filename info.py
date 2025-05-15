5c484ee94206de7fd880f364e8d2928f0e8f0576
Jk Devloper <141899550+sunil143786@users.noreply.github.com>
2025-05-15 20:32:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002193397974'))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
-PORT = os.environ.get('PORT', '5000')
+PORT = os.environ.get('PORT', '8080')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
